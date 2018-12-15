05699397bffa35e7f5d2ea09202de9215bcab938
Michal Wolowik <m.wolowik@emsyslabs.com>
2018-12-15 12:45:44 +0100

    Added missing space

--- include/configs/somlabs_visionsom_6ull.h
+++ include/configs/somlabs_visionsom_6ull.h
@@ -102,9 +102,8 @@
 	"ip_dyn=yes\0" \
 	"console=ttymxc0\0" \
 	"bootargs=console=ttymxc0,115200 "  \
-	"root=ubi0:rootfs rootfstype=ubifs " \
+	"root=ubi0:rootfs rw rootfstype=ubifs " \
 	"" \
-	"mtdparts=gpmi-nand:2m(boot),-(ubi)\0" \
 	"bootcmd=mtdparts default; ubi part ubi; ubifsmount ubi0:rootfs;" \
 	"ubifsload ${loadaddr} /boot/zImage;" \
 	"ubifsload ${fdt_addr} /boot/${fdt_file};" \
