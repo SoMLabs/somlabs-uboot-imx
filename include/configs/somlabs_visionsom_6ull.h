fce2d0cedfa9de7e4e7437612c9d247aab8eea6e
Michal Wolowik <m.wolowik@emsyslabs.com>
2018-12-15 17:59:35 +0100

    Added space at the end

--- include/configs/somlabs_visionsom_6ull.h
+++ include/configs/somlabs_visionsom_6ull.h
@@ -100,7 +100,7 @@
 	"ip_dyn=yes\0" \
 	"console=ttymxc0\0" \
 	"bootargs=console=ttymxc0,115200 ubi.mtd=ubi "  \
-	"root=ubi0:rootfs rw rootfstype=ubifs rootwait=1"		     \
+	"root=ubi0:rootfs rw rootfstype=ubifs rootwait=1 "		     \
 	"" \
 	"mtdparts=gpmi-nand:2m(boot),-(ubi)\0" \
 	"bootcmd=mtdparts default; ubi part ubi; ubifsmount ubi0:rootfs;" \
