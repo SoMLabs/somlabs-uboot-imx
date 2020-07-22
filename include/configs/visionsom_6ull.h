401fd3ca1ddedbae3a2520f4238f90a8b97676bb
Arkadiusz Karas <arkadiusz.karas@somlabs.com>
2020-07-22 22:10:40 +0200

    SOMLABS: add support for VisionSOM-6UL/ULL modules

--- include/configs/visionsom_6ull.h
+++ include/configs/visionsom_6ull.h
@@ -1,12 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright (C) 2017-2019 A. Karas, SomLabs
+ * Copyright (C) 2017-2020 A. Karas, SomLabs
  * Copyright (C) 2015-2016 Freescale Semiconductor, Inc.
  *
- * Configuration settings for the SoMlabs VisionSOM 6ULL board.
+ * Configuration settings for the SoMlabs VisionSOM 6UL/6ULL board.
  */
-#ifndef __SOMLABS_VISIONSOM_6ULL_H
-#define __SOMLABS_VISIONSOM_6ULL_H
+#ifndef __VISIONSOM_6ULL_H
+#define __VISIONSOM_6ULL_H
 
 #include <asm/arch/imx-regs.h>
 #include <linux/sizes.h>
@@ -19,19 +19,16 @@
 /* Size of malloc() pool */
 #define CONFIG_SYS_MALLOC_LEN		(16 * SZ_1M)
 
-#define CONFIG_MXC_UART
-#define CONFIG_MXC_UART_BASE		UART1_BASE
-
-/* MMC Configs */
-#ifdef CONFIG_FSL_USDHC
-#define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC2_BASE_ADDR
-
-#define CONFIG_SYS_FSL_USDHC_NUM	1
-#endif /* CONFIG_FSL_USDHC */
-
-#define CONFIG_CMD_READ
+/* for version with fastboot we set bootdelay to 0 */
+#ifdef CONFIG_FSL_FASTBOOT
+#define CONFIG_MFG_ENV_SETTINGS \
+	"bootdelay=0\0"
+#else
+#define CONFIG_MFG_ENV_SETTINGS
+#endif
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	CONFIG_MFG_ENV_SETTINGS \
 	"bootm_size=0x10000000\0" \
 	"console=ttymxc0\0" \
 	"initrd_addr=0x86800000\0" \
@@ -40,6 +37,12 @@
 	"image=zImage\0" \
 	"splashimage=0x80000000\0" \
 	"splashfile=/boot/splash.bmp\0" \
+	"setrootnand=setenv rootspec root=ubi0:rootfs ubi.mtd=ubi " \
+		"rootfstype=ubifs "CONFIG_MTDPARTS_DEFAULT"\0" \
+	"setbootscriptnand=setenv loadbootscript ubifsload " \
+		"${loadaddr} /boot/${script};\0" \
+	"setloadnand=setenv loadimage ubifsload ${loadaddr} /boot/${image}; " \
+	            "setenv loadfdt ubifsload ${fdt_addr} /boot/${fdt_file};\0" \
 	"mmcdev=1\0" \
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk1p1 rootwait rw\0" \
@@ -54,10 +57,17 @@
 	"setbootargs=setenv bootargs console=${console},${baudrate} " \
 		"${rootspec}\0" \
 	"execbootscript=echo Running bootscript...; source\0" \
-	"setfdtfile=setenv fdt_file somlabs-visionsom-6ull.dtb\0" \
-	"checkbootdev=run setbootscriptmmc; " \
+	"setfdtfile=setenv fdt_file somlabs-${board}${fdt_suffix}.dtb\0" \
+	"checkbootdev=if test ${bootdev} = nand; then " \
+		"nand device 0; ubi part ubi; ubifsmount ubi0:rootfs; " \
+		"run setbootscriptnand; " \
+		"run setrootnand; " \
+		"run setloadnand; " \
+	"else " \
+		"run setbootscriptmmc; " \
 		"run setrootmmc; " \
 		"run setloadmmc; " \
+	"fi; " \
 
 #define CONFIG_BOOTCOMMAND \
 	"run setfdtfile; " \
@@ -91,26 +101,35 @@
 #define CONFIG_SYS_INIT_SP_ADDR \
 	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
 
-/* environment organization */
-#define CONFIG_SYS_MMC_ENV_DEV		1	/* USDHC2 */
-#define CONFIG_SYS_MMC_ENV_PART		0	/* user area */
+/* NAND stuff */
+#ifdef CONFIG_NAND_MXS
+#define CONFIG_SYS_MAX_NAND_DEVICE      1
+#define CONFIG_SYS_NAND_BASE            0x40000000
+#define CONFIG_SYS_NAND_5_ADDR_CYCLE
+#define CONFIG_SYS_NAND_ONFI_DETECTION
+#define CONFIG_SYS_NAND_USE_FLASH_BBT
+#endif
+
+//#define CONFIG_ENV_SIZE			SZ_8K
 
 /* USB Configs */
 #ifdef CONFIG_CMD_USB
-#define CONFIG_EHCI_HCD_INIT_AFTER_RESET
 #define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
-#define CONFIG_MXC_USB_FLAGS   0
-#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
 #endif
 
-#ifdef CONFIG_CMD_NET
-#define CONFIG_FEC_MXC
-#define IMX_FEC_BASE			ENET_BASE_ADDR
-#define CONFIG_FEC_MXC_PHYADDR		0x1
-#define CONFIG_FEC_XCV_TYPE		RMII
-#define CONFIG_ETHPRIME			"eth0"
+#ifndef CONFIG_SPL_BUILD
+#ifdef CONFIG_DM_VIDEO
+#define CONFIG_VIDEO_MXS
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_SPLASH_SOURCE
+#define CONFIG_BMP_16BPP
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_VIDEO_BMP_GZIP
+#define CONFIG_SYS_VIDEO_LOGO_MAX_SIZE  (2 << 20)
+#endif
 #endif
 
-#define CONFIG_IMX_THERMAL
-
-#endif
+#endif /* __VISIONSOM_6ULL_H */
