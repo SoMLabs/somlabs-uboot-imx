9835fb24f6aa6135e46428d69dde29c35b103681
Arkadiusz Karas <arkadiusz.karas@somlabs.com>
2020-07-22 22:10:40 +0200

    SOMLABS: add support for VisionSOM-6UL/ULL modules

--- board/somlabs/visionsom-6ull/visionsom-6ull.c
+++ board/somlabs/visionsom-6ull/visionsom-6ull.c
@@ -25,12 +25,27 @@
 #include <linux/sizes.h>
 #include <mmc.h>
 #include <netdev.h>
+#include <splash.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 #define UART_PAD_CTRL  (PAD_CTL_PKE | PAD_CTL_PUE |		\
 	PAD_CTL_PUS_100K_UP | PAD_CTL_SPEED_MED |		\
 	PAD_CTL_DSE_40ohm   | PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+
+
+#ifdef CONFIG_LDO_BYPASS_CHECK
+void ldo_mode_set(int ldo_bypass)
+{
+	/* switch to ldo_bypass mode */
+	if (ldo_bypass) {
+		prep_anatop_bypass();
+		set_anatop_bypass(1);
+		finish_anatop_bypass();
+		printf("switch to ldo_bypass mode!\n");
+	}
+}
+#endif
 
 int dram_init(void)
 {
@@ -99,6 +114,59 @@ int board_early_init_f(void)
 	return 0;
 }
 
+#ifdef CONFIG_SPLASH_SCREEN
+static struct splash_location splash_locations[] = {
+	{
+		.name = "mmc",
+		.storage = SPLASH_STORAGE_MMC,
+		.flags = SPLASH_STORAGE_FS,
+		.devpart = "1:1",
+	},
+	{
+		.name = "nand",
+		.storage = SPLASH_STORAGE_NAND,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x300000,
+	}
+};
+
+int splash_screen_prepare(void)
+{
+	int idx = (get_boot_device() == NAND_BOOT)?1:0;
+	return splash_source_load(&splash_locations[idx], 1);
+}
+#endif
+
+#ifdef CONFIG_DM_VIDEO
+
+#define GPIO_LCD_RESET			IMX_GPIO_NR(3, 4)
+#define GPIO_LCD_BRIGHTNESS		IMX_GPIO_NR(4, 20)
+
+static int setup_lcd(void)
+{
+	enable_lcdif_clock(LCDIF1_BASE_ADDR, 1);
+
+	/* Reset the LCD */
+	gpio_request(GPIO_LCD_RESET, "lcd reset");
+	gpio_direction_output(GPIO_LCD_RESET , 0);
+	udelay(500);
+	gpio_direction_output(GPIO_LCD_RESET , 1);
+
+	/* Set Brightness to high */
+	gpio_request(GPIO_LCD_BRIGHTNESS, "backlight");
+	gpio_direction_output(GPIO_LCD_BRIGHTNESS , 1);
+
+	return 0;
+}
+
+int board_cfb_skip(void)
+{
+	/* skip cfb init */
+    return 1;
+}
+
+#endif
+
 int board_init(void)
 {
 	/* Address of boot parameters */
@@ -115,33 +183,94 @@ int board_init(void)
 	return 0;
 }
 
-#ifdef CONFIG_CMD_BMODE
-static const struct boot_mode board_boot_modes[] = {
-	/* 4 bit bus width */
-	{"sd1", MAKE_CFGVAL(0x42, 0x20, 0x00, 0x00)},
-	{"sd2", MAKE_CFGVAL(0x40, 0x28, 0x00, 0x00)},
-	{NULL,	 0},
-};
-#endif
-
 int board_late_init(void)
 {
-#ifdef CONFIG_CMD_BMODE
-	add_board_boot_modes(board_boot_modes);
-#endif
+	const char* bootdev = NULL;
+	const char* bootdevno = NULL;
+	const char* fdt_suffix = NULL;
 
 	if (is_cpu_type(MXC_CPU_MX6ULL))
 		env_set("board", "visionsom-6ull");
 	else
 		env_set("board", "visionsom-6ul");
 
+	switch (get_boot_device()) {
+		case SD1_BOOT:
+			bootdev = "sd";
+			bootdevno = "0";
+			break;
+		case MMC1_BOOT:
+			bootdev = "emmc";
+			bootdevno = "0";
+			fdt_suffix = "-emmc";
+			break;
+		case SD2_BOOT:
+			bootdev = "sd";
+			bootdevno = "1";
+			break;
+		case MMC2_BOOT:
+			bootdev = "emmc";
+			bootdevno = "1";
+			fdt_suffix = "-emmc";
+			break;
+		case QSPI_BOOT:
+			bootdev = "qspi";
+			break;
+		case NAND_BOOT:
+			bootdev = "nand";
+			fdt_suffix = "-nand";
+			break;
+		default:
+			break;
+	}
+	if(bootdev) {
+		env_set("bootdev", bootdev);
+	}
+	if(bootdevno) {
+		env_set("bootdevno", bootdevno);
+	}
+	if(fdt_suffix) {
+		env_set("fdt_suffix", fdt_suffix);
+	}
+
+#ifdef CONFIG_DM_VIDEO
+	setup_lcd();
+#endif
+
+	set_wdog_reset((struct wdog_regs *)WDOG1_BASE_ADDR);
+
 	return 0;
 }
 
-int checkboard(void)
+static int fdt_get_offset(void *fdt, const char *alias)
 {
-	printf("Board: SoMLabs VisionSOM-6UL%s\n",
-	       is_cpu_type(MXC_CPU_MX6ULL) ? "L" : "");
+	const char *path = fdt_get_alias(fdt, alias);
+	return fdt_path_offset(fdt, path);
+}
+
+static int fdt_enable_by_alias(void *fdt, const char *alias)
+{
+	int offset = fdt_get_offset(fdt, alias);
+	return fdt_status_okay(fdt, offset);
+}
+
+/*
+	This is called before relocation
+	We need to detect boot mode and configure NAND/eMMC/SD variant here
+	Nodes for not existing memories needs to be deleted from device tree.
+	This allows us to have single bootloader build independant of boot mode
+*/
+int board_fix_fdt(void *fdt)
+{
+	int boot_dev = get_boot_device();
+
+	if(boot_dev == NAND_BOOT) {
+		// enable NAND interface
+		fdt_enable_by_alias(fdt, "gpmi");
+	} else {
+		// enable USDHC2
+		fdt_enable_by_alias(fdt, "mmc1");
+	}
 
 	return 0;
 }
