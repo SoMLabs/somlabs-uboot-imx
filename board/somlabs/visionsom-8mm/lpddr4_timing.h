2125708b6b89b23e40e570f54d8c627655b5649a
Arkadiusz Karas <arkadiusz.karas@somlabs.com>
2021-08-06 15:36:24 +0200

    SOMLABS: add support for 512MiB option for VisionSOM-8MMini
    Also refactor DDR configuration data to save OCRAM space.

---
+++ board/somlabs/visionsom-8mm/lpddr4_timing.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2019 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ * Generated code from MX8M_DDR_tool
+ */
+
+#include <linux/kernel.h>
+#include <asm/arch/ddr.h>
+
+#ifndef LPDDR4_TIMING_H
+#define LPDDR4_TIMING_H
+
+/* ddr timing config params */
+extern struct dram_timing_info dram_timing_k4f6e304hbmgcj;
+extern struct dram_timing_info dram_timing_mt53b256m32d1ds;
+extern struct dram_timing_info dram_timing_mt53d512m32d2ds;
+extern struct dram_timing_info dram_timing_mt53d1024m32d4dt;
+extern struct dram_timing_info dram_timing_mt53e128m32d2ds;
+
+#endif /* LPDDR4_TIMING_H */
