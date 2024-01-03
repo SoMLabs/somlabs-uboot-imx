5a2c638255aefd7b9e7bd4389d38b228871fc19a
Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>
2024-01-03 12:13:58 +0100

    SOMLABS: change VisionSBC-8Mmini board to TitanSBC-8Mmini

--- board/somlabs/titansbc-8mmini/hw_config.h
+++ board/somlabs/titansbc-8mmini/hw_config.h
@@ -10,13 +10,13 @@
 #include <asm/arch/ddr.h>
 
 #ifdef CONFIG_SPL_BUILD
-struct dram_timing_info* visionsbc8mmini_get_dram_timing(void);
+struct dram_timing_info* titansbc8mmini_get_dram_timing(void);
 #endif
 
-const char* visionsbc8mmini_get_dram_name(void);
+const char* titansbc8mmini_get_dram_name(void);
 
-phys_size_t visionsbc8mmini_get_dram_size(void);
+phys_size_t titansbc8mmini_get_dram_size(void);
 
-const char* visionsbc8mmini_get_hw_rev_str(void);
+const char* titansbc8mmini_get_hw_rev_str(void);
 
 #endif /* HW_CONFIG_H */
