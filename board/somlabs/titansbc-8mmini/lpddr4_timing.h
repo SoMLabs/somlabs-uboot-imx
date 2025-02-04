8787aa929adca4443f9b7e7f21965ad6705a31af
Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>
2025-02-04 21:04:38 +0100

    SOMLABS: add 1GB and 4GB memory variants to titansbc-8mmini
    - add K4F8E3S4HBMFCJ memory configuration
    - add MT53E1G32D2FW memory configuration
    - refactor lpddr4_timing to fit into the bin image

---
+++ board/somlabs/titansbc-8mmini/lpddr4_timing.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2019 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ * Generated code from MX8M_DDR_tool
+ */
+
+#include <linux/kernel.h>
+#include <asm/arch/ddr.h>
+
+#ifndef LPDDR4_TIMING_H
+#define LPDDR4_TIMING_H
+
+/* ddr timing config params */
+extern struct dram_timing_info dram_timing_mt53d512m32d2ds;
+extern struct dram_timing_info dram_timing_mt53b256m32d1ds;
+extern struct dram_timing_info dram_timing_mt53d1024m32d4dt;
+extern struct dram_timing_info dram_timing_mt53e1g32d2fw;
+extern struct dram_timing_info dram_timing_k4f8e3s4hbmfcj;
+
+#endif /* LPDDR4_TIMING_H */
