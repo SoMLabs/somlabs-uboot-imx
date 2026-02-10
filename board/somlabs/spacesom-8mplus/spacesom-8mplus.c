fe1af241c92b5018d2c60307839f701410e8e52d
Krzysztof Chojnowski <krzysztof.chojnowski@somlabs.com>
2026-02-10 15:53:18 +0100

    SOMLABS: remove redundant ETH init from spacesom-8mplus

--- board/somlabs/spacesom-8mplus/spacesom-8mplus.c
+++ board/somlabs/spacesom-8mplus/spacesom-8mplus.c
@@ -427,28 +427,6 @@ int board_typec_get_mode(int index)
 #endif
 #endif
 
-static void setup_fec(void)
-{
-	struct iomuxc_gpr_base_regs *gpr =
-		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
-
-	/* Enable RGMII TX clk output */
-	setbits_le32(&gpr->gpr[1], BIT(22));
-}
-
-static int setup_eqos(void)
-{
-	struct iomuxc_gpr_base_regs *gpr =
-		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
-
-	/* set INTF as RGMII, enable RGMII TXC clock */
-	clrsetbits_le32(&gpr->gpr[1],
-			IOMUXC_GPR_GPR1_GPR_ENET_QOS_INTF_SEL_MASK, BIT(16));
-	setbits_le32(&gpr->gpr[1], BIT(19) | BIT(21));
-
-	return set_clk_eqos(ENET_125MHZ);
-}
-
 #if CONFIG_IS_ENABLED(NET)
 int board_phy_config(struct phy_device *phydev)
 {
@@ -478,14 +456,6 @@ int board_init(void)
 	imx8m_usb_power(0, true);
 	imx8m_usb_power(1, true);
 #endif
-
-	if (CONFIG_IS_ENABLED(FEC_MXC)) {
-		setup_fec();
-	}
-
-	if (CONFIG_IS_ENABLED(DWC_ETH_QOS)) {
-		setup_eqos();
-	}
 
 #ifdef CONFIG_NAND_MXS
 	setup_gpmi_nand();
