1a3320895bd59dac171c875821bedc829a153d3a
Arkadiusz Karas <arkadiusz.karas@somlabs.com>
2026-06-22 17:59:31 +0200

    SOMLABS: Add support for StarSOM-8MP

---
+++ board/somlabs/starsom-8mp/lpddr4_timing.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2026 Somlabs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ */
+
+#include <linux/kernel.h>
+#include <asm/arch/ddr.h>
+
+#ifndef LPDDR4_TIMING_H
+#define LPDDR4_TIMING_H
+
+/* ddr timing config params */
+extern struct dram_timing_info dram_timing;
+extern struct dram_timing_info dram_timing_4g;
+
+#endif /* LPDDR4_TIMING_H */
